255e1fd3311f8c7435af9fbb5758730d5ef12ff1
Krastin Krastev <krastin@hashicorp.com>
2023-12-18 17:30:37 +0100

    update gcloud dc2 with g/dc1 content

---
+++ google-cloud/dc2/gke-consul.tf
@@ -0,0 +1,24 @@
+# Create consul namespace
+resource "kubernetes_namespace" "consul" {
+  metadata {
+    name = "consul"
+  }
+}
+
+# Create Consul deployment
+resource "helm_release" "consul" {
+  name       = "consul"
+  repository = "https://helm.releases.hashicorp.com"
+  version    = var.consul_chart_version
+  chart      = "consul"
+  namespace  = "consul"
+  wait       = true
+
+  values = [
+    templatefile("${path.module}/../../k8s-yamls/consul-helm-dc2.yaml",{
+      consul_version = var.consul_version
+    })
+  ]
+
+  depends_on = [ kubernetes_namespace.consul ]
+}
