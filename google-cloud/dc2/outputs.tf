feb22e236dd0f031fd619fca49206e61502b830d
Krastin Krastev <krastin@hashicorp.com>
2023-12-19 14:28:59 +0100

    automatic hashicups install

--- google-cloud/dc2/outputs.tf
+++ google-cloud/dc2/outputs.tf
@@ -1,3 +1,7 @@
 output "get-credentials_command" {
   value = "gcloud container clusters get-credentials --zone ${var.zone} dc2"
 }
+
+output "rename-context_cmd" {
+  value = "kubectl config rename-context gke_${var.project}_${var.zone}_dc2 dc2"
+}
